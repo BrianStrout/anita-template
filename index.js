a963d85debba10c9f48a97dcbae5ad90b380f841
Brian J. Strout <brianjstrout@gmail.com>
2023-01-05 01:03:28 -0500

    uploaded avatar animatin and started hover animation of buttons

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const replay = document.querySelector(".reset-animation");
+const resetButton = document.querySelector(".reset-animation");
+const maskedImage = document.querySelector(".color-image");
+const maskedContainer = document.querySelector(".mask-container");
+const coloredTop = document.getElementById("coloredTop");
 
-replay.addEventListener("click", () => {
-  console.log("functioning");
+resetButton.addEventListener("click", () => {
+  maskedImage.classList.remove("mask-animation");
+  setTimeout(() => maskedImage.classList.add("mask-animation"), 100);
+
+  setTimeout(() => maskedContainer.classList.add("slide-framer-left"), 1750);
+  setTimeout(() => coloredTop.classList.add("visible"), 1500);
 });
