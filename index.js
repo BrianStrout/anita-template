e52fb6c9ef924f99156be87803f6121a1465f42f
Brian J. Strout <brianjstrout@gmail.com>
2023-01-06 00:06:45 -0500

    added in lotus image, made enter screen, experimented with name entrance, reorganzid js flow

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const resetButton = document.querySelector(".reset-animation");
 const maskedImage = document.querySelector(".color-image");
 const maskedContainer = document.querySelector(".mask-container");
 const coloredTop = document.getElementById("coloredTop");
+const coloredBottom = document.getElementById("color-image");
+const enterShield = document.getElementById("enterShield");
+const enterBox = document.getElementById("enter-box");
 
-resetButton.addEventListener("click", () => {
+enterShield.addEventListener("transitionend", () => {
+  introducing;
+});
+
+enterBox.addEventListener("click", () => {
+  enterShield.classList.add("touched");
+});
+
+const introducing = () => {
   maskedImage.classList.remove("mask-animation");
   setTimeout(() => maskedImage.classList.add("mask-animation"), 100);
+  setTimeout(() => {
+    maskedContainer.classList.add("slide-framer-left");
+  }, 250);
+  setTimeout(() => {
+    coloredTop.classList.add("visible");
+    coloredBottom.classList.add("hidden");
+  }, 1200);
+  setTimeout(() => {}, 2500);
+};
 
-  setTimeout(() => maskedContainer.classList.add("slide-framer-left"), 500);
-  setTimeout(() => coloredTop.classList.add("visible"), 1500);
-});
+const dropIn = () => {
+  console.log("dropping in");
+};
